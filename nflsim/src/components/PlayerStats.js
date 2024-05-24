b2b08dac7dfbf3201d9a8d4398ca102dcc7806f6
Tiernantburns <tiernan.b25@k12.xrds.org>
2024-05-23 22:26:13 -0700

    Added multiple csv

--- nflsim/src/components/PlayerStats.js
+++ nflsim/src/components/PlayerStats.js
@@ -1,29 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import Papa from 'papaparse';
+import React from 'react';
+import useCSVData from '../hooks/useCSVData';
 
-const PlayerStats = () => {
-  const [players, setPlayers] = useState([]);
-  const [headers, setHeaders] = useState([]);
-
-  useEffect(() => {
-    // Fetch the CSV file
-    fetch('csv/NFL_Rushing_Player_Ratings_-_Adjusted_for_Fumbles.csv')
-      .then(response => response.text())
-      .then(csvText => {
-        // Parse the CSV file
-        Papa.parse(csvText, {
-          header: true,
-          complete: (results) => {
-            setPlayers(results.data);
-            setHeaders(results.meta.fields);
-          },
-          error: (error) => {
-            console.error('Error parsing CSV file:', error);
-          }
-        });
-      })
-      .catch(error => console.error('Error fetching CSV file:', error));
-  }, []);
+const PlayerStats = ({ csvFilePath }) => {
+  const { data: players, headers } = useCSVData(csvFilePath);
 
   return (
     <div>
--- nflsim/src/pages/Players.js
+++ nflsim/src/pages/Players.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PlayerStats from '../components/PlayerStats';
 
-const Players = () => (
-  <div>
-    <h2>Players</h2>
-    <PlayerStats />
-  </div>
-);
+const Players = () => {
+  const [csvFilePath, setCsvFilePath] = useState(null);
+
+  const handleButtonClick = (path) => {
+    setCsvFilePath(path);
+  };
+
+  return (
+    <div>
+      <h2>Players</h2>
+      <div>
+        <button onClick={() => handleButtonClick('csv/NFL_Rushing_Player_Ratings_-_Adjusted_for_Fumbles.csv')}>
+          Rushing Stats
+        </button>
+        <button onClick={() => handleButtonClick('csv/NFL_Passing_Player_Ratings_-_Adjusted_for_Sacks.csv')}>
+          Passing Stats
+        </button>
+        <button onClick={() => handleButtonClick('csv/NFL_Rushing_Player_Ratings_-_Adjusted_for_Fumbles.csv')}>
+          Receiving Stats
+        </button>
+      </div>
+      {csvFilePath && <PlayerStats csvFilePath={csvFilePath} />}
+    </div>
+  );
+};
 
 export default Players;
